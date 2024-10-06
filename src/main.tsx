ba63437f8492fcccd71e211949fef65e8c4f5e77
rafaelx0liveira <rafaelaparecido.oliveirasilva@gmail.com>
2024-10-05 22:58:32 -0300

    feat: header organism

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,12 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-// import App from './App.tsx'
+
 import './assets/styles/_style.css'
+
+import Header from './components/organisms/Header'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    {/* <App /> */}
+    <Header />
   </StrictMode>,
 )
