6aba71f9f80698feb8df9b55f59954e2c904c774
rafaelx0liveira <rafaelaparecido.oliveirasilva@gmail.com>
2025-03-26 18:57:39 -0300

    fix: removing footer list

--- src/components/organisms/footer/Footer.tsx
+++ src/components/organisms/footer/Footer.tsx
@@ -4,21 +4,6 @@ const Footer = () => {
         <footer className="footer">
             <div className="footer-container container">
                 <h1 className="footer-title">Rafael.</h1>
-
-                <ul className="footer-list">
-                    <li>
-                        <a href="#about" className="footer-link">About</a>
-                    </li>
-
-                    <li>
-                        <a href="#portfolio" className="footer-link">Portfolio</a>
-                    </li>
-
-                    <li>
-                        <a href="#testimonials" className="footer-link">Testimonials</a>
-                    </li>
-                </ul>
-
                 <div className="footer-social">
                     <a href="https://www.linkedin.com/in/rafael-oliveira-44351b18a/" className="footer-social-link" target="_blank">
                         <i className="uil uil-linkedin"></i>
